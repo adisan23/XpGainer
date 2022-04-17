58fcac058a7fb04a464090b73fc21cda32c4693c
sebi247 <sebi452@yahoo.com>
2022-04-17 15:22:02 +0100

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/xpgainer/Profile.java
+++ app/src/main/java/com/example/xpgainer/Profile.java
@@ -3,10 +3,15 @@ package com.example.xpgainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Profile extends AppCompatActivity {
 
@@ -22,6 +27,32 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        tasksCompleted = findViewById(R.id.tasksCompleted);
+
+        ArrayList Tasks = new ArrayList<>();
+
+        Tasks.add("Run 3 miles");
+        Tasks.add("Read a book");
+        Tasks.add("Find a hobby");
+        Tasks.add("Run 3 miles");
+        Tasks.add("Run 3 miles");
+        Tasks.add("Run 3 miles");
+        Tasks.add("Run 3 miles");
+        Tasks.add("Run 3 miles");
+        Tasks.add("Run 3 miles");
+        Tasks.add("Run 3 miles");
+        Tasks.add("Run 3 miles");
+        Tasks.add("Run 3 miles");
+        Tasks.add("Run 3 miles");
+
+        ArrayAdapter<String> tasksList = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                Tasks
+        );
+
+        tasksCompleted.setAdapter(tasksList);
 
     }
 }
