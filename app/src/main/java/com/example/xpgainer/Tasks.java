9a7cff276d93fe145ac9d8e66ad552895a69dcc2
conor <conorwhelo20@gmail.com>
2022-04-19 20:11:35 +0100

    Main page change

--- app/src/main/java/com/example/xpgainer/Tasks.java
+++ app/src/main/java/com/example/xpgainer/Tasks.java
@@ -2,13 +2,56 @@ package com.example.xpgainer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Tasks extends AppCompatActivity {
+
+    private ListView availTasks;
+    private Switch activeSwitch;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
+        availTasks = findViewById(R.id.availablelist);
+        ArrayList avTasks = new ArrayList<>();
+        avTasks.add("Run 3 miles");
+        avTasks.add("Read a book");
+        avTasks.add("Find a hobby");
+        ArrayAdapter<String> avtaskList = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                avTasks
+        );
+
+
+        availTasks.setAdapter(avtaskList);
+
+        activeSwitch = (Switch) findViewById(R.id.switch2);
+
+        activeSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (activeSwitch.isChecked()) {
+
+                } else {
+
+                }
+
+            }
+        });
+
+
     }
 }
