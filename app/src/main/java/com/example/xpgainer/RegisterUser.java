f910959b40f874adf19709eb0fab254526211bc1
sebi247 <sebi452@yahoo.com>
2022-04-19 01:11:55 +0100

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/xpgainer/RegisterUser.java
+++ app/src/main/java/com/example/xpgainer/RegisterUser.java
@@ -1,5 +1,6 @@
 package com.example.xpgainer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,8 +10,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.w3c.dom.Text;
 
@@ -99,11 +105,35 @@ public class RegisterUser extends AppCompatActivity implements View.OnClickListe
        }
 
        if(password.length() < 6){
-           edtTxtPassword.setError("Password is required");
+           edtTxtPassword.setError("Min password length should be 6 characters");
            edtTxtPassword.requestFocus();
            return;
        }
 
+       mAuth.createUserWithEmailAndPassword(email,password)
+               .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                   @Override
+                   public void onComplete(@NonNull Task<AuthResult> task) {
+                       if(task.isSuccessful()){
+                           User user = new User(name, age ,email);
+
+                           FirebaseDatabase.getInstance().getReference("Users")
+                                   .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                   .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                               @Override
+                               public void onComplete(@NonNull Task<Void> task) {
+                                   if(task.isSuccessful()){
+                                       Toast.makeText(RegisterUser.this, "User has been registered succesfully", Toast.LENGTH_LONG).show();
+                                   }else{
+                                       Toast.makeText(RegisterUser.this, "Failed to register! Try again!", Toast.LENGTH_LONG).show();
+                                   }
+                               }
+                           });
+                       }else{
+                           Toast.makeText(RegisterUser.this, "Failed to register! Try again!", Toast.LENGTH_LONG).show();
+                       }
+                   }
+               });
 
     }
 
---
+++ app/src/main/java/com/example/xpgainer/User.java
@@ -0,0 +1,16 @@
+package com.example.xpgainer;
+
+public class User {
+
+    public String name, email, age;
+
+    public User(){
+
+    }
+
+    public User(String name, String age, String email){
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+}
