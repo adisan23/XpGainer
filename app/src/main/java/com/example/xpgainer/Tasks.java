3691a2a9ff5b75c289b5d64d827f212da7f903f9
conor <conorwhelo20@gmail.com>
2022-04-19 21:52:48 +0100

    Task page changes

--- app/src/main/java/com/example/xpgainer/Tasks.java
+++ app/src/main/java/com/example/xpgainer/Tasks.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -40,18 +41,25 @@ public class Tasks extends AppCompatActivity {
 
         activeSwitch = (Switch) findViewById(R.id.switch2);
 
-        activeSwitch.setOnClickListener(new View.OnClickListener() {
+
+        availTasks.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                if (activeSwitch.isChecked()) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Click List Number" + position, Toast.LENGTH_LONG).show();
+                activeSwitch.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Boolean switchState = activeSwitch.isChecked();
 
-                } else {
+                        if (activeSwitch.isChecked()) {
+                            activeSwitch.setChecked(true);
+                        } else {
+                            activeSwitch.setChecked(false);
+                        }
 
-                }
-
+                    }
+                });
             }
         });
-
-
     }
 }
